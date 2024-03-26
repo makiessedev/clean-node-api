676f3680ed0bacc435c4e4dd615dd772c8ef1979
Makiesse <makiesse.dev@gmail.com>
2024-03-26 10:22:35 +0100

    test: endure EmailValidatorAdapter returns true if validator return true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,10 +1,28 @@
+import validator from 'validator'
 import { EmailValidatorAdapter } from './email-validator'
 
 describe('Email validator', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
   it('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@email.com')
+
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+
+    const isValid = sut.isValid('invalid_email@email')
 
     expect(isValid).toBe(false)
+  })
+
+  it('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(true)
+
+    const isValid = sut.isValid('valid_email@mail.com')
+
+    expect(isValid).toBe(true)
   })
 })
